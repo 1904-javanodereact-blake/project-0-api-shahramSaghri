56318851a825030e7d022d5fa6224a71e2b26356
Your Name <you@example.com>
2019-04-19 12:09:17 -0400

    local ports is updated

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 // attach the specific users session data to req.session
 app.use(sessionMiddleware);
 
-const myPort = process.env.PORT || 8080;
+const myPort = process.env.PORT || 8081;
 
 /**
  * Register Routers
